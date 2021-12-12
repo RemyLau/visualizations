f46e94b1353014f2cdb10b54558061a424029976
RemyLau <remylau961@gmail.com>
2021-12-12 12:16:25 -0500

    Create star walker

--- graph_walk.py
+++ graph_walk.py
@@ -2,47 +2,38 @@ from manim import *
 import networkx as nx
 
 
-def get_sample_graph():
-    nodes = list(range(9))
-    edges = [
-        (0, 1),
-        (1, 2),
-        (1, 3),
-        (2, 3),
-        (3, 4),
-        (4, 5),
-        (4, 6),
-        (2, 7),
-        (7, 8),
-    ]
-    return Graph(nodes, edges, layout='spring')
-
-
-class LineExample(Scene):
-    def construct(self):
-        d = VGroup()
-        for i in range(10):
-            d.add(Dot())
-        #d.arrange_in_grid(buff=1)
-        d.arrange()
-        self.add(d)
-
-        l = Line(d[0], d[1])
-        self.add(l)
-        self.wait()
-
-        l.put_start_and_end_on(d[1].get_center(), d[2].get_center())
-        self.wait()
-
-        l.put_start_and_end_on(d[4].get_center(), d[7].get_center())
-        self.wait()
-
-
 class SampleGraph(Scene):
     def construct(self):
-        g = get_sample_graph()
+        # Create graph
+        nodes = list(range(10))
+        edges = [
+            (0, 1),
+            (1, 2),
+            (1, 3),
+            (2, 3),
+            (3, 4),
+            (4, 5),
+            (4, 6),
+            (2, 7),
+            (7, 8),
+            (8, 9),
+        ]
+        g = Graph(
+            nodes,
+            edges,
+            layout='kamada_kawai',
+            labels=True,
+            layout_scale=3.4,
+        )
         self.play(Create(g))
 
-        star = Star(5, outer_radius=0.4, density=2, color=BLUE)
+        # Create star walker
+        star = Star(n=5, outer_radius=0.5, inner_radius=0.3, density=2)
+        init_pos = 2 * RIGHT  # initial position of the star
+        star.shift(init_pos)
+        star.set_fill(BLUE, opacity=0.8)
         self.play(Create(star))
+
+        # Move walker to node 0
+        self.play(star.animate.shift(g[0].get_center() - init_pos))
         self.wait(1)
