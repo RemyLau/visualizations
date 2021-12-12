e9762b3588ca1b7cd1ec1ab635f527df5ec043ff
RemyLau <remylau961@gmail.com>
2021-12-12 17:22:08 -0500

    Refactor walker creation

--- graph_walk.py
+++ graph_walk.py
@@ -4,6 +4,15 @@ import networkx as nx
 from manim import *
 
 GRAPH_POS = 2.8 * LEFT + 0.3 * UP
+WALKER_POS = RIGHT
+
+
+def get_walker(color=BLUE, pos=WALKER_POS):
+    walker = Star(n=5, outer_radius=0.4, inner_radius=0.2, density=2, color=color)
+    walker.set_fill(color, opacity=0.7)
+    walker.shift(pos)
+    return walker
+
 
 def get_graph():
     nodes = list(range(10))
@@ -40,23 +49,20 @@ class RandomWalkOnGraph(Scene):
         init_node = 1
         walk_length = 20
         font_size = 36
-        star_pos = RIGHT
         text_pos = 2.4 * RIGHT + 0.6 * UP
 
         nxg, g = get_graph()
         self.play(Create(g))
         self.wait(2)
 
-        # Create star walker
-        star = Star(n=5, outer_radius=0.5, inner_radius=0.3, density=2)
-        star.shift(star_pos)
-        star.set_fill(BLUE, opacity=0.8)
-        self.play(Create(star))
+        # Create walker
+        walker = get_walker()
+        self.play(Create(walker))
         self.wait(2)
 
         # Move walker to node 0
-        relative_shift = g[init_node].get_center() - star.get_center()
-        self.play(star.animate.shift(relative_shift))
+        relative_shift = g[init_node].get_center() - walker.get_center()
+        self.play(walker.animate.shift(relative_shift))
         self.wait(2)
 
         text1 = Text(r"Real random walker", font_size=font_size)
@@ -67,10 +73,9 @@ class RandomWalkOnGraph(Scene):
         for i in range(walk_length):
             cur_node = random.choice(list(nxg[cur_node]))
 
-            relative_shift = g[cur_node].get_center() - star.get_center()
-            self.play(star.animate.shift(relative_shift), run_time=0.8)
+            relative_shift = g[cur_node].get_center() - walker.get_center()
+            self.play(walker.animate.shift(relative_shift), run_time=0.8)
             self.wait(0.1)
-
 
             if i == 1:
                 self.add(text1.shift(text_pos))
