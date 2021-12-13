3b4e04041eeecb2bd16d4b91960af814f7da4e7e
RemyLau <remylau961@gmail.com>
2021-12-12 19:50:14 -0500

    Add RecordSingleRandomWalk scene

--- graph_walk.py
+++ graph_walk.py
@@ -5,11 +5,11 @@ from manim import *
 
 GRAPH_POS = 2.8 * LEFT + 0.3 * UP
 WALKER_POS = RIGHT
-WALK_LENGTH = 15
+WALK_LENGTH = 24
 TEST = False
 
 
-def get_walker(color=BLUE, pos=WALKER_POS):
+def get_walker(pos=WALKER_POS, color=BLUE):
     walker = Star(n=5, outer_radius=0.4, inner_radius=0.2, density=2, color=color)
     walker.set_fill(color, opacity=0.7)
     walker.shift(pos)
@@ -90,9 +90,9 @@ class RandomWalkOnGraph(Scene):
 
 
 class RandomWalk(Scene):
-    def setup_scene(self, init_node=1):
+    def setup_scene(self, init_node=1, walker_color=BLUE):
         self.nxg, self.g = get_graph()
-        self.walker = get_walker()
+        self.walker = get_walker(pos=init_node, color=walker_color)
         self.walker.shift(
             self.g[init_node].get_center() - self.walker.get_center()
         )
@@ -144,3 +144,64 @@ class TransitionProbability(RandomWalk):
 
         self.add(eqn4)
         self.wait(1 if not TEST else 0.1)
+
+
+class RecordSingleRandomWalk(RandomWalk):
+    def construct(self):
+        random.seed(0)
+        font_size = 36
+        txt_pos = 2.4 * RIGHT + 0.6 * UP
+        walk_hist_pos = 1.2 * RIGHT + 2.5 * UP
+        walk_hist_font_size = 29
+        walk_hist_record_length = 9
+        walker_color = BLUE
+        init_node = 1
+
+        txt1 = Text(r"Real random walker", font_size=font_size)
+        txt2 = Text(r"Not an actor", font_size=font_size)
+
+        self.setup_scene(init_node=init_node, walker_color=walker_color)
+
+        # Generate random walk starting from init node
+        cur_node = init_node
+        self.add(
+            Text(
+                str(init_node),
+                font_size=walk_hist_font_size,
+                color=walker_color,
+            ).shift(walk_hist_pos)
+        )
+        self.wait(1 if not TEST else 0.1)
+
+        for i in range(WALK_LENGTH if not TEST else 12):
+            cur_node = random.choice(list(self.nxg[cur_node]))
+
+            relative_shift = self.g[cur_node].get_center() - self.walker.get_center()
+            self.play(self.walker.animate.shift(relative_shift), run_time=0.8)
+            self.wait(0.1)
+
+            if i < walk_hist_record_length:
+                # Record walk history
+                head = Text(
+                    str(cur_node),
+                    font_size=walk_hist_font_size,
+                    color=walker_color,
+                ).shift(walk_hist_pos + (i + 1) * 0.4 * RIGHT)
+                self.add(head)
+                self.wait(0.1)
+            elif i == walk_hist_record_length:
+                head = Tex(
+                    r"\dots",
+                    font_size=walk_hist_font_size,
+                    color=walker_color,
+                ).shift(walk_hist_pos + (i + 1) * 0.4 * RIGHT)
+                self.add(head)
+                self.wait(0.1)
+
+            if i == 5:
+                self.add(txt1.shift(txt_pos))
+                self.wait(0.1)
+
+            if i == 9:
+                self.add(txt2.shift(txt_pos + 0.6 * DOWN))
+                self.wait(0.1)
